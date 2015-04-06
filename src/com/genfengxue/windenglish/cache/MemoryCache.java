0469140780816fbea2ee414de6a3ad752f15b3e5
Jack Sun <Jacksunwei@gmail.com>
2015-04-06 22:32:39 +0800

    resolve #1 MemoryCache implementation

--- src/com/genfengxue/windenglish/cache/MemoryCache.java
+++ src/com/genfengxue/windenglish/cache/MemoryCache.java
@@ -1,18 +1,21 @@
 package com.genfengxue.windenglish.cache;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoryCache<Key, Value> {
 
+	private Map<Key, Value> cache = new HashMap<Key, Value>();
+
 	public boolean contains(Key key) {
-		// TODO
-		return false;
+		return cache.containsKey(key);
 	}
 
 	public Value get(Key key) {
-		// TODO
-		return null;
+		return cache.get(key);
 	}
 
 	public void put(Key key, Value value) {
-		// TODO
+		cache.put(key, value);
 	}
 }
