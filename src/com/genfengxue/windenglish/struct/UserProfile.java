2bcca13f3240837019c5df03cdc249aab43440cf
Jack Sun <Jacksunwei@gmail.com>
2015-04-11 17:04:46 +0800

    resolve #4 user profile structure

--- src/com/genfengxue/windenglish/struct/UserProfile.java
+++ src/com/genfengxue/windenglish/struct/UserProfile.java
@@ -1,36 +1,46 @@
 package com.genfengxue.windenglish.struct;
 
 public class UserProfile {
-	private String userName;
-	private String name;
+	public static final int USER_ROLE_STUDENT = 1;
+	public static final int USER_ROLE_TEACHER = 2;
+	public static final int USER_ROLE_ADMIN = 3;
 
+	private int userNo;
+	private int role;
+	private String nickName;
+	private String avatar;
 	private String email;
 
-	public UserProfile() {
+	public UserProfile(int userNo, int role, String nickName, String avatar,
+			String email) {
+		this.userNo = userNo;
+		this.role = role;
+		this.nickName = nickName;
+		this.avatar = avatar;
+		this.email = email;
 	}
 
 	public String getName() {
-		return name;
+		return nickName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public int getUserNo() {
+		return userNo;
 	}
 
 	public String getEmail() {
 		return email;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public int getRole() {
+		return role;
 	}
 
+	public String getNickName() {
+		return nickName;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
 }
