26cefd8b32ac8ce10b5303aa4117bac0269be7aa
Jack Sun <Jacksunwei@gmail.com>
2015-04-16 00:01:28 +0800

    resolve #24 activity for profile submission

---
+++ src/com/genfengxue/windenglish/activities/ProfileActivity.java
@@ -0,0 +1,13 @@
+package com.genfengxue.windenglish.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfileActivity extends Activity {
+	
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+}
