d2e358ed51b72b4bc343ef9f26127b9f22585bd8
Jack Sun <Jacksunwei@gmail.com>
2015-04-18 19:13:39 +0800

    #31 add swipe back detection to go to courseActivity

--- src/com/genfengxue/windenglish/activities/CourseActivity.java
+++ src/com/genfengxue/windenglish/activities/CourseActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -58,6 +59,8 @@ public class CourseActivity extends Activity {
 				CourseActivity.this.finish();
 			}
 		});
+		
+		((ImageView) findViewById(R.id.refreshBtn)).setVisibility(View.INVISIBLE);
 		
 		new UpdateCourseTask().execute();
 	}
