23143ddd05e27bb5b58083134beb7f03f0f8f28d
lutaoact <lutaoact@hotmail.com>
2015-04-23 15:45:58 +0800

    webView.goBack(); v1.0.2

--- src/com/genfengxue/windenglish/activities/WebviewActivity.java
+++ src/com/genfengxue/windenglish/activities/WebviewActivity.java
@@ -1,7 +1,9 @@
 package com.genfengxue.windenglish.activities;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -10,6 +12,7 @@ import com.genfengxue.windenglish.R;
 public class WebviewActivity extends Activity {
 	private WebView webView;
 	
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +20,16 @@ public class WebviewActivity extends Activity {
 		
 		webView = (WebView)findViewById(R.id.webview);
 		webView.setWebViewClient(new WebViewClient());
+		webView.getSettings().setJavaScriptEnabled(true);
 		webView.loadUrl("http://weiqing.genfengxue.com/app/index.php?i=2&c=entry&id=11&do=detail&m=news&wxref=mp.weixin.qq.com#wechat_redirect");
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+			webView.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 }
